17e36e13df6525c3b8d16b4f4511cea3fa6892a1
rafael <rafaelscatarino@hotmail.com>
2023-03-25 23:04:21 -0300

    exercicio tkinter

---
+++ Tkinter/aula1janela.py
@@ -0,0 +1,14 @@
+from tkinter import *
+
+#cores
+cor1 ="#2595EB" #azul
+
+janela = Tk()
+janela.title("Ola Mundo")
+janela.geometry("500x250")
+janela.config(bg = cor1)
+
+#janela.iconphoto(False, PhotoImage(file = 'git-150px.png'))
+janela.resizable(width=False,height=False)
+
+janela.mainloop()
---
+++ Tkinter/aula5entry.py
@@ -0,0 +1,24 @@
+from tkinter import *
+
+janela = Tk()
+janela.title("Entry")
+janela.geometry("250x250")
+janela.config(bg = "black")
+
+label_nome = Label(janela, width=10, height=2, text="nome : ", bg="black", fg="#FFFFFF")
+label_nome.grid(row=0, column=0)
+
+entry_nome = Entry(janela, width=15)
+entry_nome.grid(row=0, column=1)
+
+label_idade = Label(janela, width=10, height=2, text="idade : ", bg="black", fg="#FFFFFF")
+label_idade.grid(row=1, column=0)
+
+entry_idade = Entry(janela, width=15)
+entry_idade.grid(row=1, column=1)
+
+button_enviar = Button(janela, width=10, height=0, text="Enviar")
+button_enviar.place(x="65", y="90")
+
+
+janela.mainloop()
